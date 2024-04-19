8b27c9ea0af82b74d7e16ba001b8abf9307cca01
kazuma102 <22fi102horie@gmail.com>
2024-04-19 12:16:21 +0900

    4/19 12:16

---
+++ java-basic-1/src/IfTest3.java
@@ -0,0 +1,24 @@
+//IfTest3 xが7,8,9ながbignumber
+//xが4,5,6ならmiddle number
+//xが1,2,3ならsmall numberを表示するプログラムを書け
+//xがそれ以外(10や-1なら)ならout of scopeと表示するプログラムをかけ
+
+public class IfTest3 {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        /* 以下を改造する。 **/
+        if (x >= 7) {
+            System.out.println(x + " is Big Number");
+        }
+        else if(x >= 4){
+            System.out.println(x + " is middle number");
+        }
+        else if(x >= 1){
+            System.out.println(x + " is small number");
+        }
+        else{
+            System.out.println(x + " is out of scope");
+        }
+
+    }// main end
+}// class end
