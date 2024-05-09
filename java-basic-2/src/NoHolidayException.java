2f6909e04ec960cc882db0f9d486e49f9608a0fa
kazuma102 <22fi102horie@gmail.com>
2024-05-09 18:49:34 +0900

    kadai3

--- java-basic-2/src/NoHolidayException.java
+++ java-basic-2/src/NoHolidayException.java
@@ -4,6 +4,13 @@ public class NoHolidayException extends Exception {
     @Override
     public void printStackTrace() {
         super.printStackTrace();
-        System.err.println("この日は平日だよ！はたらきたくないねー。エラーメッセージです。");
+        System.err.println("報酬の課題だ…持っていきたまえ\r\n" + //
+                        "　　　　　　　　∧＿∧\r\n" + //
+                        "　　　　　　　⊂(･ω･`)つ-､\r\n" + //
+                        "　　　　　　課題/　　/_/　|\r\n" + //
+                        "　　　 　　 課題ヽノ L|／\r\n" + //
+                        "　　　 ／￣ 課題 ￣￣／|\r\n" + //
+                        "　　／＿＿＿＿＿＿／ | |\r\n" + //
+                        "　　||―――――ｰ||");
     }
 }
